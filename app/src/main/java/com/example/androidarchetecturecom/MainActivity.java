0df4d2ac2e001a0c521e9c047b3ee07a4e0186c9
tjose <tijojosewc@gmail.com>
2019-04-12 16:44:51 -0500

    finished creating viewModels and RoomDB

--- app/src/main/java/com/example/androidarchetecturecom/MainActivity.java
+++ app/src/main/java/com/example/androidarchetecturecom/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.androidarchetecturecom;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "ON CHANGED", Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
